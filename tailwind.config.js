e3937be4b492b5cdceaf8437b1bf1dc0f1502259
Ben H <deepfreespace@gmail.com>
2022-03-09 18:00:12 -0700

    File cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'theme-gray': '#FCFAFA',
+                'theme-lilac': '#9F9FED',
+                'theme-orange': '#F5853F',
+                'theme-purple': '#190E4F',
+                'theme-pink': '#E83F6F',
+            },
+        },
+    },
+    plugins: [],
+};
