f29a14572df8884fd4ac3fb0814a164d7b5a1adf
Ben H <deepfreespace@gmail.com>
2022-03-28 17:31:23 -0600

    Redesigned header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,56 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = ({ currentPage, setCurrentPage }) => {
-    // Get viewport width
-    const vw = Math.max(
-        document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
-    );
-
-    const hamburgerToggle = () => {
-        document
-            .getElementById('hamburger-top')
-            .classList.toggle('button-transform-top-active');
-        document
-            .getElementById('hamburger-middle')
-            .classList.toggle('button-transform-middle-active');
-        document
-            .getElementById('hamburger-bottom')
-            .classList.toggle('button-transform-bottom-active');
-        document.getElementById('nav-list').classList.toggle('navlist-active');
-        if (vw < 640) {
-            document
-                .getElementById('header-top')
-                .classList.toggle('navlist-active');
-        } else {
-            document
-                .getElementById('my-name')
-                .classList.toggle('navlist-active');
-        }
-    };
-
     return (
-        <header id="nav-header" className="shadow-sm bg-theme-purple">
-            {/* Header top */}
-            <div
-                id="header-top"
-                className="w-full flex flex-col justify-between items-center p-2 transition-all duration-300"
-            >
-                {/* Logo */}
-                <div className="flex justify-center items-center text-2xl h-20">
-                    <img
-                        src="./assets/images/logo.svg"
-                        alt="Benjamin Holt monogram"
-                        className="h-full z-20"
-                        id="logo-image-mobile"
-                    />
-                </div>
-            </div>
-            {/* Header bottom/Nav */}
-            <nav className="w-full flex flex-wrap justify-around">
+        <header id="nav-header" className="shadow-sm py-3 bg-theme-purple">
+            <nav className="w-full flex justify-center">
                 <ul
                     id="nav-list"
-                    className="w-full flex justify-evenly pb-1 text-theme-lilac transition-all duration-300 md:text-lg"
+                    className="flex flex-col grow justify-evenly pr-2 text-right text-theme-lilac transition-all duration-300 md:text-lg"
                 >
                     <li>
                         <Link
@@ -67,6 +23,28 @@ const Header = ({ currentPage, setCurrentPage }) => {
                     </li>
                     <li>
                         <Link
+                            to="/contact"
+                            onClick={() => setCurrentPage('contact')}
+                            className={`hover:text-theme-pink transition-all duration-300 ${
+                                currentPage === 'contact' && 'text-theme-pink'
+                            }`}
+                        >
+                            //Contact
+                        </Link>
+                    </li>
+                </ul>
+                {/* Logo */}
+                <div className="flex justify-center items-center text-2xl h-20">
+                    <img
+                        src="./assets/images/logo.svg"
+                        alt="Benjamin Holt monogram"
+                        className="h-full z-20"
+                        id="logo-image-mobile"
+                    />
+                </div>
+                <ul className="flex flex-col grow justify-evenly pl-3 text-left text-theme-lilac transition-all duration-300 md:text-lg2">
+                    <li>
+                        <Link
                             to="/work"
                             onClick={() => setCurrentPage('work')}
                             className={`hover:text-theme-pink transition-all duration-300 ${
@@ -76,8 +54,6 @@ const Header = ({ currentPage, setCurrentPage }) => {
                             //Work
                         </Link>
                     </li>
-                </ul>
-                <ul className="w-full flex justify-evenly pb-3 text-theme-lilac transition-all duration-300 md:text-lg2">
                     <li>
                         <Link
                             to="/resume"
@@ -87,17 +63,6 @@ const Header = ({ currentPage, setCurrentPage }) => {
                             }`}
                         >
                             //Resume
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to="/contact"
-                            onClick={() => setCurrentPage('contact')}
-                            className={`hover:text-theme-pink transition-all duration-300 ${
-                                currentPage === 'contact' && 'text-theme-pink'
-                            }`}
-                        >
-                            //Contact
                         </Link>
                     </li>
                 </ul>
