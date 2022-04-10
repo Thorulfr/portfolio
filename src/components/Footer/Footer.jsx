ae7610b21262e34f5f743631e73cb6203e10bea2
Ben H <deepfreespace@gmail.com>
2022-04-09 23:23:49 -0600

    Fixed scrollbar position

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className="flex flex-col items-center">
+        <footer className="flex flex-col items-center pt-5">
             <div className="flex justify-around items-center pt-3 w-3/4 lg:w-1/3">
                 {/* LinkedIn logo + link */}
                 <a
