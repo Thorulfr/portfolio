083ef618622d83b810237cb14c0cca2ddeb308f5
Ben H <deepfreespace@gmail.com>
2022-04-19 19:27:26 -0600

    Skewed dividers in work section

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -18,23 +18,23 @@ const Work = ({ setCurrentPage }) => {
             <div className="px-4 text-theme-lilac md:px-12 lg:px-32 xl:px-32 xl:grid xl:grid-cols-2 xl:my-6 2xl:px-60">
                 <div>
                     <Project project={projects.typePlusPlus} />
-                    <div className="border-2 border-theme-pink mx-14 rounded-full"></div>
+                    <div className="border-2 border-theme-pink mx-14 origin-left -skew-x-[14deg]"></div>
                 </div>
                 <div>
                     <Project project={projects.numberOneFan} />
-                    <div className="border-2 border-theme-pink mx-14 rounded-full"></div>
+                    <div className="border-2 border-theme-pink mx-14 origin-left -skew-x-[14deg]"></div>
                 </div>
                 <div>
                     <Project project={projects.streamability} />
-                    <div className="border-2 border-theme-pink mx-14 rounded-full"></div>
+                    <div className="border-2 border-theme-pink mx-14 origin-left -skew-x-[14deg]"></div>
                 </div>
                 <div>
                     <Project project={projects.weatherDashboard} />
-                    <div className="border-2 border-theme-pink mx-14 rounded-full"></div>
+                    <div className="border-2 border-theme-pink mx-14 origin-left -skew-x-[14deg]"></div>
                 </div>
                 <div>
                     <Project project={projects.budgetTracker} />
-                    <div className="border-2 border-theme-pink mx-14 rounded-full xl:hidden"></div>
+                    <div className="border-2 border-theme-pink mx-14 origin-left -skew-x-[14deg] xl:hidden"></div>
                 </div>
                 <Project project={projects.socialNetworkAPI} />
             </div>
