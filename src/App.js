6c2087f77c429f123a2fb7d5fbb3c8ccfa52e6ab
Ben H <deepfreespace@gmail.com>
2022-09-05 10:47:14 -0600

    HashRouter test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Imports
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Component imports
 import Nav from './components/Header';
