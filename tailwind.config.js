76bd857ee9d9d627bc7c02a3454eda6ab2f55631
Ben H <deepfreespace@gmail.com>
2022-10-16 17:41:58 -0600

    Refactor cursor styles to use @apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = {
                 'theme-purple-light': '#342B6E',
                 'theme-pink': '#E83F6F',
             },
+            cursor: {
+                'digital-standard':
+                    'url(/public/assets/cursors/standard-cursor.svg), auto',
+                'digital-pointer':
+                    'url(/public/assets/cursors/pointer-cursor.svg), auto',
+                'digital-text':
+                    'url(/public/assets/cursors/text-cursor.svg), auto',
+            },
         },
     },
     plugins: [],
